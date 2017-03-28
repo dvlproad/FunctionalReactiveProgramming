4204dc930a54023a6be4861523bc14ae26f3b436
dvlproad <studyroad@qq.com>
2017-03-28 16:34:14 +0800

    【Feature】init 和 Cocoapods导入ReactiveCocoa5.0

---
+++ RACDemo/RACDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACDemo
+//
+//  Created by 李超前 on 2017/3/28.
+//  Copyright © 2017年 dvlproad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
